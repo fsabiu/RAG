eabc6fc862edb39ba5772eada522316371ccde2d
fsabiu <fsabiu@gmail.com>
2024-10-18 20:09:35 +0000

    Chat model class defined and implemented

--- src/rag_app/core/implementations/chat_model/oci_chat_model.py
+++ src/rag_app/core/implementations/chat_model/oci_chat_model.py
@@ -1,9 +1,11 @@
 import logging
+from abc import ABC, abstractmethod
 from langchain_community.chat_models import ChatOCIGenAI
 from langchain_core.prompts import PromptTemplate
-from typing import Optional, Iterator, Union
+from typing import Optional, Iterator, Union, AsyncIterator
 import oci
 from rag_app.core.interfaces.chat_model_interface import ChatModelInterface
+from ...interfaces.conversation_interface import Conversation
 
 # Enable debug logging for the entire oci package
 #logging.getLogger('oci').setLevel(logging.DEBUG)
@@ -13,7 +15,52 @@ oci.base_client.is_http_log_enabled(True)
 
 logger = logging.getLogger(__name__)
 
-class OCI_CommandRplus(ChatModelInterface):
+class ChatModel(ChatModelInterface):
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @property
+    @abstractmethod
+    def llm(self):
+        pass
+
+    async def chat(self, system_prompt: str, query: str, conversation: Optional[Conversation] = Conversation(), stream: bool = False) -> Union[str, AsyncIterator[str]]:
+        prompt_template = f"{system_prompt}\n\n"
+        
+        if conversation:
+            prompt_template += f"{conversation.get_formatted_history()}\n\n"
+        
+        prompt_template += "User: {query}\n\nAssistant:"
+        prompt = PromptTemplate(input_variables=["query"], template=prompt_template)
+        
+        # Log the final prompt
+        logger.info(f"History: {conversation.get_formatted_history()}\n\n")
+        logger.info(f"Final prompt: {prompt.format(query=query)}")
+        
+        llm_chain = prompt | self.llm
+
+        if stream:
+            return self._stream_response(llm_chain, query)
+        else:
+            return await self._generate_response(llm_chain, query)
+
+    async def _generate_response(self, llm_chain, query: str) -> str:
+        logger.info(f"Generating response with {self.__class__.__name__} for query: {query[:50]}...")
+        response = await llm_chain.ainvoke(query)
+        
+        return response
+
+    async def _stream_response(self, llm_chain, query: str) -> AsyncIterator[str]:
+        logger.info(f"Streaming response with {self.__class__.__name__} for query: {query[:50]}...")
+        full_response = ""
+        
+        async for chunk in llm_chain.astream(query):
+            if chunk.content is not None:
+                full_response += chunk.content
+                yield chunk.content
+
+class OCI_CommandRplus(ChatModel, ChatModelInterface):
     def __init__(self, settings: dict):
         logger.info("Initializing OCI_CommandRplus chat model")
         
@@ -31,36 +78,29 @@ class OCI_CommandRplus(ChatModelInterface):
             }
         }
         
-        # Unpack the dictionary into the ChatOCIGenAI constructor
-        self.llm = ChatOCIGenAI(**llm_params)
+        self._llm = ChatOCIGenAI(**llm_params)
 
-    def chat(self, system_prompt: str, query: str, stream: bool = False) -> Union[str, Iterator[str]]:
-        prompt_template = f"{system_prompt}\n\nHuman: {{query}}\n\nAssistant:"
-        prompt = PromptTemplate(input_variables=["query"], template=prompt_template)
-        llm_chain = prompt | self.llm
+    @property
+    def llm(self):
+        return self._llm
 
-        if stream:
-            return self._stream_response(llm_chain, query)
-        else:
-            return self._generate_response(llm_chain, query)
-
-    def _generate_response(self, llm_chain, query: str) -> str:
-        logger.info(f"Generating response with OCI_CommandRplus for query: {query[:50]}...")
-        response = llm_chain.invoke(query)
-        return response
-
-    def _stream_response(self, llm_chain, query: str) -> Iterator[str]:
-        logger.info(f"Streaming response with OCI_CommandRplus for query: {query[:50]}...")
-        for chunk in llm_chain.stream(query):
-            yield chunk
-
-class OCI_Llama3_70(ChatModelInterface):
-    def __init__(self):
+class OCI_Llama3_70(ChatModel, ChatModelInterface):
+    def __init__(self, settings: dict):
         logger.info("Initializing OCI_Llama3-70 chat model")
-        # Add any specific initialization for OCI_Llama3-70
-        self.api_key = settings.OCI_API_KEY
+        llm_params = {
+            "model_id": settings["MODEL_ID_LLAMA3"],
+            "service_endpoint": settings["OCI_GENAI_ENDPOINT"],
+            "compartment_id": settings["OCI_COMPARTMENT_ID"],
+            "auth_type": "API_KEY",
+            "auth_profile": settings["OCI_CONFIG_PROFILE"],
+            "model_kwargs": {
+                "temperature": settings["TEMPERATURE"],
+                "max_tokens": settings["MAX_TOKENS"],
+                "top_p": settings["TOP_P"]
+            }
+        }
+        self._llm = ChatOCIGenAI(**llm_params)
 
-    def chat(self, system_prompt: str, query: str, stream: bool = False) -> Union[str, Iterator[str]]:
-        logger.info(f"Generating response with OCI_Llama3-70 for prompt: {query[:50]}...")
-        # Implement the logic to generate a response using OCI_Llama3-70
-        pass
+    @property
+    def llm(self):
+        return self._llm
